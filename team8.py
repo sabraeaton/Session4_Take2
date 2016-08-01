b1ef6c5df3ff8ec39a6e3ecc7247d2a5535a1ad6
Sabra Eaton <seaton@sdale.org>
2016-08-01 16:05:43 -0500

    Update team8.py

--- team8.py
+++ team8.py
@@ -17,7 +17,7 @@ def move(my_history, their_history, my_score, their_score):
     Make my move.
     Returns 'c' or 'b'. 
     '''
-
+# this is sabra making a change
     # my_history: a string with one letter (c or b) per round that has been played with this opponent.
     # their_history: a string of the same length as history, possibly empty. 
     # The first round between these two players is my_history[0] and their_history[0].
